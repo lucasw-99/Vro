f1a3df0d6eef8ce45f9565fc3c3673663a2d6ff1
Lucas Wotton <lucas@98point6.com>
2018-05-29 08:36:56 -0700

    Add Google cloud functions folder.

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const algoliasearch = require('algoliasearch');
+const algolia = algoliasearch(functions.config().algolia.appid, functions.config().algolia.adminkey);
+
+exports.updateEvents = functions.database.ref('/events/{eventId}').onWrite((event, context) => {
+    console.log('event:', event)
+    console.log('params:', context)
+    const data = event.after.val()
+    const index = algolia.initIndex('events')
+    const eventId = context.params['eventId']
+    if (!data) {
+        return index.deleteObject(eventId, (err) => {
+            if (err) { 
+                console.log('err:', err)
+                return false
+            }
+            console.log('Event Removed from Algolia Index with id:', eventId)
+            return true
+          })
+    }
+
+    data['objectID'] = eventId
+    return index.saveObject(data, (err, content) => {
+        if (err) { 
+            console.log('err:', err)
+            return false
+        }
+        console.log('Event Updated in Algolia Index with id:', data.objectID)
+        return true
+    })    
+})
