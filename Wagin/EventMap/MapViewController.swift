586ffebc8a7cb38ee164948305b98558d5e956c7
Lucas Wotton <lucas@98point6.com>
2018-05-31 08:23:57 -0700

    Add uislider to adjust the search radius 😁 Next step is to add an MKCircle to show all events inside the radius!

--- Wagin/EventMap/MapViewController.swift
+++ Wagin/EventMap/MapViewController.swift
@@ -15,6 +15,8 @@ import InstantSearchCore
 class MapViewController: UIViewController {
     private let headerView = UIView()
     private let headerLabel = UILabel()
+    private let radiusSlider = UISlider()
+    private let currentRadiusLabel = UILabel()
 
     private let locationManager = CLLocationManager()
     private var currentCoordinate: CLLocationCoordinate2D?
@@ -22,7 +24,8 @@ class MapViewController: UIViewController {
     private let searchBarWidget = SearchBarWidget()
     private let statsWidget = StatsLabelWidget()
     private let mapViewWidget = MapViewWidget()
-    private let sliderWidget = SliderWidget()
+
+    private var searchRadius: Int = 10000  // in meters
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +42,7 @@ class MapViewController: UIViewController {
     }
 }
 
+// MARK: User location
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         manager.stopUpdatingLocation()
@@ -49,12 +53,33 @@ extension MapViewController: CLLocationManagerDelegate {
     }
 }
 
+// MARK: Search bar
 extension MapViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.endEditing(true)
         let query = searchBar.text
         print("query: \(query!)")
+        // TODO: Change user location
         findEvents(userLocation: currentCoordinate!)
+    }
+}
+
+// MARK: Slider
+extension MapViewController {
+    @objc private func sliderValueDidChange(_ slider: UISlider) {
+        print("Slider value changed: \(slider.value)")
+        let currentValue = Int(round(slider.value))
+        currentRadiusLabel.text = "\(currentValue)m"
+    }
+
+    @objc private func sliderDidTouchUp(_ slider: UISlider) {
+        let currentValue = Int(round(slider.value))
+        if currentValue != searchRadius {
+            print("searching events")
+            searchRadius = currentValue
+            InstantSearch.shared.searcher.params.aroundRadius = Query.AroundRadius.explicit(UInt(searchRadius))
+            findEvents(userLocation: currentCoordinate!)
+        }
     }
 }
 
@@ -62,7 +87,7 @@ extension MapViewController: UISearchBarDelegate {
 extension MapViewController {
     private func setupSubviews() {
         headerLabel.text = "Nearby Events"
-        headerLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        headerLabel.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         headerLabel.numberOfLines = 1
         headerLabel.textAlignment = .center
         headerView.addSubview(headerLabel)
@@ -70,6 +95,20 @@ extension MapViewController {
         statsWidget.textAlignment = .center
         statsWidget.font = UIFont.systemFont(ofSize: 14)
         headerView.addSubview(statsWidget)
+
+        radiusSlider.minimumValue = 100
+        radiusSlider.maximumValue = 1000000
+        radiusSlider.value = Float(searchRadius)
+        radiusSlider.isContinuous = true
+        radiusSlider.tintColor = .blue
+        radiusSlider.addTarget(self, action: #selector(MapViewController.sliderValueDidChange(_:)), for: .valueChanged)
+        radiusSlider.addTarget(self, action: #selector(MapViewController.sliderDidTouchUp(_:)), for: .touchUpInside)
+        headerView.addSubview(radiusSlider)
+
+        currentRadiusLabel.text = "\(searchRadius)m"
+        currentRadiusLabel.textAlignment = .center
+        currentRadiusLabel.font = UIFont.systemFont(ofSize: 12)
+        headerView.addSubview(currentRadiusLabel)
 
         view.addSubview(headerView)
 
@@ -91,17 +130,29 @@ extension MapViewController {
             make.top.equalTo(view.layoutMarginsGuide.snp.topMargin)
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
-            make.height.equalTo(90)
+            make.height.equalTo(125)
         }
 
         headerLabel.snp.makeConstraints { make in
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
-            make.centerY.equalToSuperview()
+            make.top.equalToSuperview().offset(20)
         }
 
         statsWidget.snp.makeConstraints { make in
             make.top.equalTo(headerLabel.snp.bottom).offset(10)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+
+        radiusSlider.snp.makeConstraints { make in
+            make.top.equalTo(statsWidget.snp.bottom).offset(10)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(250)
+        }
+
+        currentRadiusLabel.snp.makeConstraints { make in
+            make.top.equalTo(radiusSlider.snp.bottom)
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
         }
@@ -124,7 +175,7 @@ extension MapViewController {
     private func setupInstantSearch() {
         // TODO: Make this cleaner please, this is dirty dirty
         InstantSearch.shared.configure(appID: "1W1CGWYI4N", apiKey: "99390aeab22b4e94b41e432939f3d424", index: "events")
-        InstantSearch.shared.searcher.params.aroundRadius = Query.AroundRadius.explicit(UInt(10000))
+        InstantSearch.shared.searcher.params.aroundRadius = Query.AroundRadius.explicit(UInt(searchRadius))
     }
 
     private func registerWidgets() {
