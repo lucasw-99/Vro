6a7f4f305a1bbc4be96f03cb3f5a62a8602e5bf3
Lucas Wotton <lucas.wotton@rubrik.com>
2018-07-27 08:02:46 -0700

    Add user profile cell with a hack :( Still looks much better than the old way though.

---
+++ Vro/Global/UserProfile/ProfileHeaderCollectionViewCell.swift
@@ -0,0 +1,147 @@
+//
+//  ProfileHeaderCollectionViewCell.swift
+//  Vro
+//
+//  Created by Lucas Wotton on 7/26/18.
+//  Copyright © 2018 Lucas Wotton. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ProfileHeaderCollectionViewCell: UICollectionViewCell {
+    private let profilePhotoView = UIImageView()
+    private let followerStatsLabel = UILabel()
+    private let followButton = UIButton()
+    private let separatorView2 = UIView()
+    
+    private let currentUser: UserProfile
+    private var followerCount: Int
+    private var followingCount: Int
+    
+    var selectedUser: UserProfile! {
+        didSet {
+            updateUI()
+        }
+    }
+
+    override init(frame: CGRect) {
+        guard let user = UserService.currentUserProfile else { fatalError("Current user is nil fuk") }
+        self.currentUser = user
+        self.followerCount = 0
+        self.followingCount = 0
+        super.init(frame: frame)
+        setupSubviews()
+        setupLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+// MARK: Setup subviews
+private extension ProfileHeaderCollectionViewCell {
+    private func setupSubviews() {
+        contentView.addSubview(profilePhotoView)
+        
+        // TODO: Weird stuff going on with this not showing up..
+        followerStatsLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        followerStatsLabel.textColor = UIColor.black
+        followerStatsLabel.numberOfLines = 0
+        followerStatsLabel.textAlignment = .center
+        followerStatsLabel.text = "\(followerCount) follower\(followerCount != 1 ? "s" : "")\n\(followingCount) following"
+        contentView.addSubview(followerStatsLabel)
+        
+        followButton.setTitle("Follow", for: .normal)
+        followButton.setTitle("Following", for: .selected)
+        followButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        followButton.setTitleColor(.black, for: .normal)
+        followButton.addTarget(self, action: #selector(ProfileHeaderCollectionViewCell.followButtonPressed(_:)), for: .touchUpInside)
+        Util.roundedCorners(ofColor: .black, element: followButton)
+        self.followButton.isUserInteractionEnabled = false
+        contentView.addSubview(followButton)
+        
+        separatorView2.backgroundColor = .gray
+        contentView.addSubview(separatorView2)
+    }
+    
+    private func setupLayout() {
+        profilePhotoView.snp.makeConstraints { make in
+            make.width.equalTo(100)
+            make.height.equalTo(100)
+            make.top.equalToSuperview().offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        followerStatsLabel.snp.makeConstraints { make in
+            make.top.equalTo(profilePhotoView.snp.bottom).offset(20)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        followButton.snp.makeConstraints { make in
+            make.top.equalTo(followerStatsLabel.snp.bottom).offset(30)
+            make.width.equalTo(100)
+            make.height.equalTo(40)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    private func updateUI() {
+        // don't show follow button if you're looking up your own profile
+        followButton.isHidden = currentUser.uid == selectedUser.uid ? true : false
+        
+        separatorView2.snp.makeConstraints { make in
+            let isCurrentUser = currentUser.uid == selectedUser.uid
+            make.top.equalTo(isCurrentUser ? followerStatsLabel.snp.bottom : followButton.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+            make.height.equalTo(1)
+            make.bottom.equalToSuperview()
+        }
+        
+        ImageService.getImage(withURL: selectedUser.photoURL) { image in
+            guard let image = image else { fatalError("User image was nil") }
+            self.profilePhotoView.image = image
+            Util.makeImageCircular(image: self.profilePhotoView, 100)
+        }
+        
+        let selectedUserFollowersPath = String(format: Constants.Database.userFollowerInfo, selectedUser.uid)
+        let selectedUserFollowersRef = Database.database().reference().child(selectedUserFollowersPath)
+        
+        FollowersService.getFollowerInfo(selectedUser.uid, selectedUserFollowersRef) { selectedUserFollowerInfo in
+            print("UserProfileViewController selectedUser observable")
+            self.followerCount = selectedUserFollowerInfo.followers.count
+            self.followingCount = selectedUserFollowerInfo.following.count
+            self.followerStatsLabel.text = "\(self.followerCount) follower\(self.followerCount != 1 ? "s" : "")\n\(self.followingCount) following"
+            let currentUser = Follower(self.currentUser.uid)
+            self.followButton.isSelected = selectedUserFollowerInfo.followers.contains(currentUser)
+            self.followButton.isUserInteractionEnabled = true
+            // TODO: Make use of the fact that the followers shit is an observable?
+            selectedUserFollowersRef.removeAllObservers()
+        }
+    }
+}
+
+// MARK: Button functions
+extension ProfileHeaderCollectionViewCell {
+    @objc private func followButtonPressed(_ sender: Any) {
+        print("BUTTON FUCKING pressed")
+        // disable button while editing
+        Util.toggleButton(button: self.followButton, isEnabled: false)
+        guard let currentUser = UserService.currentUserProfile else { fatalError("current user nil??") }
+        print("follow button disabled")
+        let wasSelected = followButton.isSelected
+        followButton.isSelected = !followButton.isSelected
+        print("updating followers")
+        FollowersService.updateFollowers(uid: currentUser.uid, followedUid: selectedUser.uid, addFollower: !wasSelected) {
+            DispatchQueue.main.async {
+                self.followerCount += wasSelected ? -1 : 1
+                Util.toggleButton(button: self.followButton, isEnabled: true)
+                self.followerStatsLabel.text = "\(self.followerCount) follower\(self.followerCount != 1 ? "s" : "")\n\(self.followingCount) following"
+            }
+        }
+    }
+}
--- Vro/Global/UserProfile/UserProfileViewController.swift
+++ Vro/Global/UserProfile/UserProfileViewController.swift
@@ -14,10 +14,7 @@ import SnapKit
 // code duplication by making Collection view a class?
 class UserProfileViewController: UIViewController {
     private let selectedUser: UserProfile
-    private var selectedUserFollowers: UserFollowers!
-    private let selectedUserFollowersRef: DatabaseReference
-    private let currentUserUid: String
-
+    
     private let headerView = UIView()
     private let backButton = UIButton()
     private let usernameLabel = UILabel()
@@ -45,32 +42,10 @@ class UserProfileViewController: UIViewController {
     
     private var dataSource = [EventPost]()
 
-    // TODO: Add selectable followers and following, possibly in scroll view. Maybe
-    // make it linkable text??
 
     init(_ userProfile: UserProfile) {
         selectedUser = userProfile
-        
-        guard let uid = UserService.currentUserProfile?.uid else { fatalError("current user nil") }
-        currentUserUid = uid
-        let selectedUserFollowersPath = String(format: Constants.Database.userFollowerInfo, selectedUser.uid)
-        selectedUserFollowersRef = Database.database().reference().child(selectedUserFollowersPath)
-        
         super.init(nibName: nil, bundle: nil)
-
-        // disable follow button until observable fires
-        followButton.isUserInteractionEnabled = false
-
-        // TODO: Put this code in viewDidDisappear and remove the DatabaseReferences
-        FollowersService.getFollowerInfo(selectedUser.uid, selectedUserFollowersRef) { selectedUserFollowerInfo in
-            print("UserProfileViewController selectedUser observable")
-            self.selectedUserFollowers = selectedUserFollowerInfo
-            self.setupFollowingLabelText()
-            let currentUser = Follower(self.currentUserUid)
-            self.followButton.isSelected = selectedUserFollowerInfo.followers.contains(currentUser)
-            self.followButton.isUserInteractionEnabled = true
-        }
-        
         observeEventPosts()
     }
 
@@ -103,35 +78,12 @@ extension UserProfileViewController {
         separatorView.backgroundColor = .gray
         view.addSubview(separatorView)
         
-        ImageService.getImage(withURL: selectedUser.photoURL) { image in
-            guard let image = image else { fatalError("User image was nil") }
-            self.profilePhotoView.image = image
-            Util.makeImageCircular(image: self.profilePhotoView, 100)
-        }
-        view.addSubview(profilePhotoView)
-
-        followerStatsLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
-        followerStatsLabel.numberOfLines = 0
-        followerStatsLabel.textAlignment = .center
-        view.addSubview(followerStatsLabel)
-
-        followButton.setTitle("Follow", for: .normal)
-        followButton.setTitle("Following", for: .selected)
-        followButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
-        followButton.setTitleColor(.black, for: .normal)
-        followButton.addTarget(self, action: #selector(UserProfileViewController.followButtonPressed(_:)), for: .touchUpInside)
-        // don't show follow button if you're looking up your own profile
-        followButton.isHidden = UserService.currentUserProfile!.uid == selectedUser.uid ? true : false
-        Util.roundedCorners(ofColor: .black, element: followButton)
-        view.addSubview(followButton)
-        
-        separatorView2.backgroundColor = .gray
-        view.addSubview(separatorView2)
-        
         eventsCollectionView.delegate = self
         eventsCollectionView.dataSource = self
         eventsCollectionView.refreshControl = refresher
         eventsCollectionView.register(EventPostCollectionViewCell.self, forCellWithReuseIdentifier: "EventPostCell")
+        // TODO: HACK. One day I should fix this. https://stackoverflow.com/questions/17681183/uicollectionview-header-and-footer-view
+        eventsCollectionView.register(ProfileHeaderCollectionViewCell.self, forCellWithReuseIdentifier: "UserProfileHeader")
         view.addSubview(eventsCollectionView)
 
         view.backgroundColor = .white
@@ -164,57 +116,22 @@ extension UserProfileViewController {
             make.trailing.equalToSuperview().offset(-10)
             make.height.equalTo(1)
         }
-
-        profilePhotoView.snp.makeConstraints { make in
-            make.width.equalTo(100)
-            make.height.equalTo(100)
-            make.top.equalTo(separatorView.snp.bottom).offset(20)
-            make.centerX.equalToSuperview()
-        }
-
-        followerStatsLabel.snp.makeConstraints { make in
-            make.top.equalTo(profilePhotoView.snp.bottom).offset(20)
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-        }
-
-        followButton.snp.makeConstraints { make in
-            make.top.equalTo(followerStatsLabel.snp.bottom).offset(30)
-            make.width.equalTo(100)
-            make.height.equalTo(40)
-            make.centerX.equalToSuperview()
-        }
-        
-        separatorView2.snp.makeConstraints { make in
-            let isCurrentUser = currentUserUid == selectedUser.uid
-            make.top.equalTo(isCurrentUser ? followerStatsLabel.snp.bottom : followButton.snp.bottom).offset(20)
-            make.leading.equalToSuperview().offset(10)
-            make.trailing.equalToSuperview().offset(-10)
-            make.height.equalTo(1)
-        }
         
         eventsCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(separatorView2.snp.bottom)
+            make.top.equalTo(separatorView.snp.bottom)
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
             make.bottom.equalToSuperview()
         }
-    }
-
-    private func setupFollowingLabelText() {
-        let followerCount = selectedUserFollowers.followers.count
-        let followingCount = selectedUserFollowers.following.count
-        followerStatsLabel.text = "\(followerCount) follower\(followerCount != 1 ? "s" : "")\n\(followingCount) following"
     }
 }
 
 // MARK: Populate dataSource
 extension UserProfileViewController: UICollectionViewDataSource {
     @objc private func observeEventPosts() {
-        guard let currentUid = UserService.currentUserProfile?.uid else { fatalError("Current user is nil") }
-        
-        EventPostService.getUserEvents(currentUid) { eventPosts in
+        EventPostService.getUserEvents(selectedUser.uid) { eventPosts in
             self.dataSource = eventPosts
+            print("dataSource for userProfile: \(eventPosts)")
             // Do UI updating on main thread
             DispatchQueue.main.async {
                 self.eventsCollectionView.reloadData()
@@ -235,21 +152,7 @@ extension UserProfileViewController {
     @objc private func backButtonPressed(_ sender: UIButton) {
         // Stop observing updates to user followers
         print("back button pressed")
-        selectedUserFollowersRef.removeAllObservers()
         navigationController?.popViewController(animated: true)
-    }
-
-    @objc private func followButtonPressed(_ sender: Any) {
-        // disable button while editing
-        Util.toggleButton(button: self.followButton, isEnabled: false)
-        guard let currentUser = UserService.currentUserProfile else { fatalError("current user nil??") }
-        print("follow button disabled")
-        let wasSelected = followButton.isSelected
-        followButton.isSelected = !followButton.isSelected
-        print("updating followers")
-        FollowersService.updateFollowers(uid: currentUser.uid, followedUid: selectedUserFollowers.uid, addFollower: !wasSelected) {
-            Util.toggleButton(button: self.followButton, isEnabled: true)
-        }
     }
 }
 
@@ -354,27 +257,38 @@ extension UserProfileViewController: EventPostCellDelegate {
 // MARK: Collection view
 extension UserProfileViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return dataSource.count
+        return dataSource.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // TODO: Change later to always return 1, check if it still works
-        return dataSource.isEmpty ? 0 : 1
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.section == 0 {
+            // TODO: Hack. Need to eventually fix this?
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UserProfileHeader", for: indexPath) as! ProfileHeaderCollectionViewCell
+            cell.selectedUser = selectedUser
+            return cell
+        }
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EventPostCell", for: indexPath) as! EventPostCollectionViewCell
-        let eventPost = dataSource[indexPath.section]
+        let eventPost = dataSource[indexPath.section - 1]
         cell.eventPost = eventPost
         cell.buttonDelegate = self
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let eventPost = dataSource[indexPath.section]
+        let zeroHeightSize = CGSize(width: collectionView.frame.width - 10 - 10, height: 0)
+        if indexPath.section == 0 {
+            let sizingCell = ProfileHeaderCollectionViewCell()
+            sizingCell.selectedUser = selectedUser
+            let size = sizingCell.contentView.systemLayoutSizeFitting(zeroHeightSize, withHorizontalFittingPriority: .required, verticalFittingPriority: .defaultLow)
+            return size
+        }
+        let eventPost = dataSource[indexPath.section - 1]
         let sizingCell = EventPostCollectionViewCell()
         sizingCell.eventPost = eventPost
-        let zeroHeightSize = CGSize(width: collectionView.frame.width - 10 - 10, height: 0)
         let size = sizingCell.contentView.systemLayoutSizeFitting(zeroHeightSize, withHorizontalFittingPriority: .required, verticalFittingPriority: .defaultLow)
         return size
     }
@@ -383,4 +297,3 @@ extension UserProfileViewController: UICollectionViewDelegate, UICollectionViewD
         return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
 }
-
